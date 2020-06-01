fc2040d6fb8964483e791c3db432e8b3846f944a
marcof <m.fagiani85@gmail.com>
2020-06-02 00:14:41 +0200

    add run ML pipeline script

--- run_ml_pipeline.sh
+++ run_ml_pipeline.sh
@@ -1,1 +1,1 @@
-python data/process_data.py data/disaster_messages.csv data/disaster_categories.csv data/DisasterResponse.db
+python models/train_classifier.py data/DisasterResponse.db models/classifier.pkl
